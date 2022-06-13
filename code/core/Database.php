5e557e7ce253744c4bdad88d8c9acb359e490a8b
Paula Malina <pastraguspaula@gmail.com>
2022-06-13 17:14:25 +0300

    commit 3

--- code/core/Database.php
+++ code/core/Database.php
@@ -85,10 +85,17 @@ class Database
         {
             require_once 'migrations/' . $migration . '.php';
             $instance = new $migration();
+            $this->deleteMigrationFromDb($migration);
             $this->log("Deleting migration $migration" . PHP_EOL);
             $instance->down();
             $this->log("Deleted migration $migration" . PHP_EOL);
         }
         $this->log("All given migrations were deleted" . PHP_EOL);
+    }
+    private function deleteMigrationFromDb($migration)
+    {
+        $migration=$migration.'.php';
+        $statement = $this->pdo->prepare("DELETE FROM migrations WHERE migration = '" . $migration . "';");
+        $statement->execute();
     }
 }
