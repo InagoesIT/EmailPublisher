4c22864696e3cc549408932f91e5bcba1994c16c
Paula Malina <pastraguspaula@gmail.com>
2022-06-15 23:47:01 +0300

    Add user in db at receiving new email

--- code/controllers/MailController.php
+++ code/controllers/MailController.php
@@ -2,7 +2,9 @@
 
 namespace app\controllers;
 
+use app\core\Database;
 use function MongoDB\BSON\toJSON;
+use app\models\User;
 
 class MailController extends Controller
 {
@@ -22,17 +24,30 @@ class MailController extends Controller
             echo "NO NEW EMAILS";
         else
             foreach ($mails as $msg_number) {
-                // Get email headers and body
 
                 $path='C:\\xampp\\htdocs\\emailPublisher\\mail_files\\' . $msg_number . 'mail.html';
 
                 $header = imap_headerinfo($conn, $msg_number);
+
+//                echo "personal " . $header->sender[0]->mailbox . "<br>";
+//                echo "fromaddress " . $header->sender[0]->host . "<br>";
+
+                $myEmail=$header->sender[0]->mailbox . '@' . $header->sender[0]->host;
+
+                echo $myEmail . "<br>";
+                if(!User::verifyIfEmailExists($myEmail)){
+                    $user= new User();
+                    $user->setEmail($myEmail);
+                    $user->setIsActive(true);
+                    $user->setToken('1234567');
+                    $user->save();
+                }
+
                 error_reporting(E_ALL ^ E_NOTICE);
                 $body = imap_fetchbody($conn, $msg_number, "2");
                 $printable = imap_qprint($body);
 
                 echo $header->subject . "<br>";
-//                echo imap_qprint($body);
 
                 file_put_contents($path, $printable);
 
--- code/models/User.php
+++ code/models/User.php
@@ -7,159 +7,176 @@ use app\core\DbModel;
 
 class User extends DbModel
 {
-	public ?int $id = null;
-	public string $email = "";
-	public bool $isActive = false;
-	public ?string $token;
+    public ?int $id = null;
+    public string $email = "";
+    public bool $isActive = false;
+    public ?string $token;
 
-	public function __construct()
-	{
-		$arguments = func_get_args();
-		$numberOfArguments = func_num_args();
 
-		if (method_exists($this, $function = '__construct'.$numberOfArguments)) {
-			call_user_func_array(array($this, $function), $arguments);
-		}
-	}
+    public function __construct()
+    {
+        $arguments = func_get_args();
+        $numberOfArguments = func_num_args();
 
-	public function __construct1($email)
-	{
-		$this->email = $email;
-	}
+        if (method_exists($this, $function = '__construct' . $numberOfArguments)) {
+            call_user_func_array(array($this, $function), $arguments);
+        }
+    }
 
-	public function __construct2($email, $token)
-	{
-		$this->token = $token;
-	}
+    public function __construct1($email)
+    {
+        $this->email = $email;
+    }
 
-	/**
-	 * @param string $email
-	 */
-	public function setEmail(string $email): void
-	{
-		$this->email = $email;
-	}
+    public function __construct2($email, $token)
+    {
+        $this->token = $token;
+    }
 
-	public static function tableName(): string
-	{
-		return 'users';
-	}
+    /**
+     * @param string $email
+     */
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
 
-	public static function attributes(): array
-	{
-		return ['email', 'isActive', 'token'];
-	}
+    public static function tableName(): string
+    {
+        return 'users';
+    }
 
-	public function rules(): array
-	{
-		return [
-			'email' => [self::RULE_REQUIRED, self::RULE_EMAIL]
-		];
-	}
+    public static function attributes(): array
+    {
+        return ['email', 'isActive', 'token'];
+    }
 
-	public function save(): bool
-	{
-		$this->isActive = false;
-		return parent::save();
-	}
+    public function rules(): array
+    {
+        return [
+            'email' => [self::RULE_REQUIRED, self::RULE_EMAIL]
+        ];
+    }
 
-	public function login(): bool
-	{
-		$user = User::findOne(['email' => $this->email]);
-		$this->id = $user->id;
-		$this->activateUser();
+    public function save(): bool
+    {
+        $this->isActive = false;
+        return parent::save();
+    }
 
-		//add the user id to the session so we know who is logged in
-		return App::$app->login($this);
-	}
+    public function login(): bool
+    {
+        $user = User::findOne(['email' => $this->email]);
+        $this->id = $user->id;
+        $this->activateUser();
 
-	public function generateToken(): void
-	{
-		$this->token = bin2hex(openssl_random_pseudo_bytes(4));
-	}
+        //add the user id to the session so we know who is logged in
+        return App::$app->login($this);
+    }
 
-	public function updateToken()
-	{
-		$statement = self::prepare("UPDATE users SET token = '" . $this->token . "' 
+    public function generateToken(): void
+    {
+        $this->token = bin2hex(openssl_random_pseudo_bytes(4));
+    }
+
+    public function updateToken()
+    {
+        $statement = self::prepare("UPDATE users SET token = '" . $this->token . "' 
 										WHERE email = '" . $this->email . "';");
-		$statement->execute();
-	}
+        $statement->execute();
+    }
 
-	public function activateUser()
-	{
-		$this->isActive = true;
-		$statement = self::prepare("UPDATE users SET isActive = '" . $this->isActive . "' 
+    public function activateUser()
+    {
+        $this->isActive = true;
+        $statement = self::prepare("UPDATE users SET isActive = '" . $this->isActive . "' 
 										WHERE email = '" . $this->email . "';");
-		$statement->execute();
-	}
+        $statement->execute();
+    }
 
-	public function isTokenValid(string $token) : bool
-	{
-		$user = self::findOne(["email" => $this->email]);
-		echo $user->email;
-		echo $user->token;
+    public function isTokenValid(string $token): bool
+    {
+        $user = self::findOne(["email" => $this->email]);
+        echo $user->email;
+        echo $user->token;
 
-		return ($user->token == $token);
-	}
+        return ($user->token == $token);
+    }
 
-	/**
-	 * @return int|null
-	 */
-	public function getId(): ?int
-	{
-		return $this->id;
-	}
 
-	/**
-	 * @param int|null $id
-	 */
-	public function setId(?int $id): void
-	{
-		$this->id = $id;
-	}
+    public static function verifyIfEmailExists($myEmail) : ?bool
+    {
+        $statement = self::prepare('SELECT * from users');
+        $statement->execute();
+        if ($statement->rowCount() > 0) {
+          while($row=$statement->fetch()){
+//              echo "email" . $row['email'] . "<br>";
+              if($row['email']==$myEmail){
+                  return true;
+              }
+          }
+        }
+        return false;
+    }
 
-	/**
-	 * @return bool
-	 */
-	public function isActive(): bool
-	{
-		return $this->isActive;
-	}
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
 
-	/**
-	 * @param bool $isActive
-	 */
-	public function setIsActive(bool $isActive): void
-	{
-		$this->isActive = $isActive;
-	}
+    /**
+     * @param int|null $id
+     */
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
 
-	/**
-	 * @return string|null
-	 */
-	public function getToken(): ?string
-	{
-		return $this->token;
-	}
+    /**
+     * @return bool
+     */
+    public function isActive(): bool
+    {
+        return $this->isActive;
+    }
 
-	/**
-	 * @param string|null $token
-	 */
-	public function setToken(?string $token): void
-	{
-		$this->token = $token;
-	}
+    /**
+     * @param bool $isActive
+     */
+    public function setIsActive(bool $isActive): void
+    {
+        $this->isActive = $isActive;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getEmail(): string
-	{
-		return $this->email;
-	}
+    /**
+     * @return string|null
+     */
+    public function getToken(): ?string
+    {
+        return $this->token;
+    }
 
-	public static function primaryKey(): string
-	{
-		return 'id';
-	}
+    /**
+     * @param string|null $token
+     */
+    public function setToken(?string $token): void
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public static function primaryKey(): string
+    {
+        return 'id';
+    }
 }
