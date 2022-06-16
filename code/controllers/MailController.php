92fe0e2fb65f1238965ade663e41c0ee6fd5f20a
Raul-Madalin <raul05madalin@yahoo.com>
2022-06-16 17:57:10 +0300

    Added subject preocess and client mail response

--- code/controllers/MailController.php
+++ code/controllers/MailController.php
@@ -4,11 +4,164 @@ namespace app\controllers;
 
 use app\core\Database;
 use app\models\Publication;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
 use function MongoDB\BSON\toJSON;
 use app\models\User;
 
 class MailController extends Controller
 {
+    private const HOST = "{imap.gmail.com:993/imap/ssl/novalidate-cert/norsh}INBOX";
+    private const USER = "emailpublisherweb@gmail.com";
+    private const PASSWORD = "lqfalrpltsljnbwx";
+    private const PATTERN = "/(\[{1}).*?(\]{1})/i";
+    private const INVALID_SUBJECT = "Invalid Subject";
+    private const VALID_SUBJECT = "Valid Subject";
+
+    private $conn;
+
+    public function setupInbox()
+    {
+        $this->conn = imap_open(self::HOST, self::USER, self::PASSWORD) or die("unable to connect Gmail: " . imap_last_error());
+        $mails = imap_search($this->conn, 'UNSEEN');
+
+        return $mails;
+    }
+
+    public function checkUser($pub, $msg_number)
+    {
+        $header = imap_headerinfo($this->conn, $msg_number);
+        $senderAddress = $header->sender[0]->mailbox . '@' . $header->sender[0]->host;
+
+        if(!User::verifyIfEmailExists($senderAddress) && $senderAddress!='emailpublisherweb@gmail.com'){
+            $user= new User();
+            $user->setEmail($senderAddress);
+            $user->setIsActive(true);
+            $user->setToken('1234567'); // TODO generate token
+            $user->save();
+
+            $pub->setIdUser($user->getId());
+        }
+        else
+            $pub->setIdUser(User::getUserIdByEmail($senderAddress));
+    }
+
+    public function checkSubject($pub, $msg_number): string
+    {
+        // TODO all echos to be replaced with actions accordingly
+        $header = imap_headerinfo($this->conn, $msg_number);
+        $subject = $header->subject;
+
+        $tags = preg_match_all(self::PATTERN, $subject, $matches);
+        for($it = 0; $it < $tags; $it++) {
+            $tag = preg_split("[=]", $matches[0][$it]);
+            if ($tag[0] != "[password" && $tag[0] != "[duration" && $tag[0] != "[public]" && $tag[0] != "[private]")
+                return self::INVALID_SUBJECT;
+            else {
+                switch ($tag[0]) {
+                    case "[password":
+                        $password = substr($tag[1], 0, strlen($tag[1]) - 1);
+                        $pub->setPassword($password);
+                        break;
+                    case "[duration":
+                        $duration = preg_split("/[dhm]/", $tag[1]);
+                        if (strpos($tag[1], "d") > strpos($tag[1], "h") ||
+                            strpos($tag[1], "d") > strpos($tag[1], "m") ||
+                            strpos($tag[1], "h") > strpos($tag[1], "m"))
+                            return self::INVALID_SUBJECT;
+                        else {
+                            echo "<br>";
+                            if (str_contains($tag[1], "d") && str_contains($tag[1], "h") && str_contains($tag[1], "m")) {
+                                echo "Days: " . $duration[0] . "<br>";
+                                echo "Hours: " . $duration[1] . "<br>";
+                                echo "Minutes: " . $duration[2] . "<br>";
+                            }
+                            else if (str_contains($tag[1], "d") && str_contains($tag[1], "h")) {
+                                echo "Days: " . $duration[0] . "<br>";
+                                echo "Hours: " . $duration[1] . "<br>";
+                            }
+                            else if (str_contains($tag[1], "d") && str_contains($tag[1], "m")) {
+                                echo "Days: " . $duration[0] . "<br>";
+                                echo "Minutes: " . $duration[1] . "<br>";
+                            }
+                            else if (str_contains($tag[1], "h") && str_contains($tag[1], "m")) {
+                                echo "Hours: " . $duration[0] . "<br>";
+                                echo "Minutes: " . $duration[1] . "<br>";
+                            }
+                            else if (str_contains($tag[1], "m")) {
+                                echo "Minutes: " . $duration[0] . "<br>";
+                            }
+                        }
+                        break;
+                    case "[public]":
+                        $pub->setIsPublic(true);
+                        break;
+                    case "[private]":
+                        $pub->setIsPublic(false);
+                        break;
+                }
+            }
+        }
+        return self::VALID_SUBJECT;
+    }
+
+    public function sendMail ($address, $subject, $body)
+    {
+        $mail = new PHPMailer(true);
+
+        try {
+            // Server settings
+            $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+            $mail->isSMTP();
+            $mail->Host = 'smtp.gmail.com';
+            $mail->SMTPAuth = true;
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+            $mail->Port = 587;
+
+            $mail->Username = 'emailpublisherweb@gmail.com'; // YOUR gmail email
+            $mail->Password = 'lqfalrpltsljnbwx'; // YOUR gmail password
+
+            // Sender and recipient settings
+            $mail->setFrom($mail->Username, 'EmailPublisher');
+            $mail->addAddress($address, $address);
+
+            // Setting the email content
+//            $mail->IsHTML(true);
+            $mail->Subject = $subject;
+            $mail->Body = $body;
+            $mail->AltBody = $body;
+
+            $mail->send();
+        } catch (Exception $e) {
+
+        }
+    }
+
+    public function processInbox()
+    {
+        $pub = new Publication();
+        $mails = $this->setupInbox();
+        if ($mails !== NULL)
+            foreach ($mails as $msg_number) {
+                $this->checkUser($pub, $msg_number);
+                $goodMail = $this->checkSubject($pub, $msg_number);
+
+                $header = imap_headerinfo($this->conn, $msg_number);
+                $senderAddress = $header->sender[0]->mailbox . '@' . $header->sender[0]->host;
+
+                echo "<br><br><br>" . $goodMail . "<br><br><br>";
+
+                if ($goodMail == self::VALID_SUBJECT)     // TODO publication link
+                    $this->sendMail($senderAddress, $goodMail,
+                        "Thank you for publishing your mail.\n");
+                else
+                    $this->sendMail($senderAddress, $goodMail,
+                        "We are sorry to inform you, your mail was not published.\n");
+            }
+    }
+
+    // TODO remove code below when not useful anymore, function never called
     public function getMail()
     {
 
@@ -41,7 +194,7 @@ class MailController extends Controller
                     $user= new User();
                     $user->setEmail($myEmail);
                     $user->setIsActive(true);
-                    $user->setToken('1234567');
+                    $user->setToken('1234567'); // TODO generate token
                     $user->save();
                 }
 
