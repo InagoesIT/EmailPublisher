4a0bb0a01a2e6d5da3ff3d6208cb3b14bf627bf6
ina <ina@debian-BULLSEYE-live-builder-AMD64>
2022-06-20 22:31:48 +0300

    added help to welcome

--- code/views/welcome.php
+++ code/views/welcome.php
@@ -10,7 +10,7 @@ if (isset($_POST['email']))
     <P>Do you want to publish your emails? Well, our site is your solution!</P>
     <img src="images/welcome.png" alt="1 person holds an email and the other one looks at a site">
     <div class="row">
-        <button class="button" id="button1">Get info</button>
+        <button class="button" id="button1" onclick="window.location.href='/help'">Help</button>
         <button class="button" id="button2" onclick="window.location.href='/auth'">Get started</button>
     </div>
 </div>
